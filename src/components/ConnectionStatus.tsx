feb14c557f3b4e59b844de020f8cd0ba2ef2bbfc
Niccolò Maltoni <nicco.mlt@gmail.com>
2020-02-16 20:09:55 +0100

    feat: add EventBus connection component
    add a component that let the user set the backend which exposes a Vert.x EventBus via SockJS

--- src/components/ConnectionStatus.tsx
+++ src/components/ConnectionStatus.tsx
@@ -2,43 +2,93 @@ import {
   Typography,
   Toolbar,
   ToolbarProps,
-  Tooltip,
+  TextField,
+  Button,
+  CircularProgress,
 } from '@material-ui/core';
-import { CheckCircle, HelpOutline, Cancel } from '@material-ui/icons';
-import React, { useEffect } from 'react';
+import { CheckCircle, Cancel } from '@material-ui/icons';
+import React, { ReactElement, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { Autocomplete } from '@material-ui/lab';
 import { RootState } from '../app/rootReducer';
 import { ebConnect } from '../utils/EventBusMiddleware';
+import { EventBusStatus } from '../features/render/execSlice';
 
-const ConnectionStatus: React.FC<Exclude<ToolbarProps, ['children', 'container', 'item']>> = (props) => {
-  const connection: 'open' | 'closed' = useSelector((state: RootState) => state.exec.connection);
-  const connectionIcon: () => JSX.Element = () => {
+const ConnectionStatus: React.FC = (props: Exclude<ToolbarProps, ['children', 'container', 'item']>) => {
+  const connection: EventBusStatus = useSelector((state: RootState) => state.exec.connection);
+
+  function connectionIcon(): ReactElement {
     switch (connection) {
       case 'open':
         return <CheckCircle color="inherit" />;
       case 'closed':
         return <Cancel color="inherit" />;
+      case 'pending':
       default:
-        return <HelpOutline color="inherit" />;
+        return <CircularProgress color="inherit" size={24} />;
     }
-  };
+  }
 
   const dispatch = useDispatch();
 
-  useEffect(() => {
-    dispatch(ebConnect({ host: '/eventbus/' }));
-  }, [connection, dispatch]);
+  const [backends, setBackends] = useState(!process.env.NODE_ENV || process.env.NODE_ENV === 'development' ? [
+    'http://localhost:8080/eventbus/',
+    'https://protelis-web.herokuapp.com/eventbus/',
+    'https://protelis-web-develop.herokuapp.com/eventbus/',
+  ] : [
+    'https://protelis-web.herokuapp.com/eventbus/',
+    'https://protelis-web-develop.herokuapp.com/eventbus/',
+  ]);
+
+  const [selected, setSelected] = useState(backends[0]);
 
   return (
-
-    <Tooltip title={connection === 'open' ? 'Execution is running' : 'Run some code to connect to server'} arrow>
-      <Toolbar {...props}>
-        <Typography variant="h6">
-          Connection status:&nbsp;
-        </Typography>
-        {connectionIcon()}
-      </Toolbar>
-    </Tooltip>
+    <Toolbar {...props}>
+      <Typography variant="h6">
+        Connection:&nbsp;
+      </Typography>
+      <Typography variant="h6">
+        <Autocomplete
+          id="backend-selector"
+          color="inherit"
+          freeSolo
+          options={backends}
+          value={selected}
+          getOptionLabel={(option: string) => option}
+          onChange={(event: React.ChangeEvent<{}>, value: string | null) => {
+            setSelected(value?.trim() || '');
+          }}
+          style={{ width: 450 }}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              variant="outlined"
+              fullWidth
+              onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                setSelected(event.target.value?.trim() || '');
+              }}
+            />
+          )}
+        />
+      </Typography>
+      <Button
+        variant="contained"
+        aria-label="connect"
+        startIcon={connectionIcon()}
+        onClick={() => {
+          let choice = selected.trim();
+          if (choice[choice.length - 1] !== '/') {
+            choice += '/';
+          }
+          if (backends.filter((value) => value.trim() === choice).length <= 0) {
+            setBackends([choice, ...backends]);
+          }
+          return dispatch(ebConnect({ host: selected }));
+        }}
+      >
+        Connect
+      </Button>
+    </Toolbar>
   );
 };
 
