bf384b3a0b14ca00246ab77070a142963181f81e
Niccolò Maltoni <nicco.mlt@gmail.com>
2020-02-17 11:18:38 +0100

    build(storybook): add TSDocgen

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,22 @@
+const path = require('path');
+
 module.exports = {
-  stories: ['../src/**/*.stories.js'],
+  stories: ['../src/stories/**/*.stories.(ts|tsx|js|jsx|mdx)'],
   addons: [
-    '@storybook/preset-create-react-app',
+    {
+      name: '@storybook/preset-create-react-app',
+      options: {
+        tsDocgenLoaderOptions: {
+          tsconfigPath: path.resolve(__dirname, '../tsconfig.json')
+        }
+      }
+    },
+    {
+      name: '@storybook/addon-docs',
+      options: {
+        configureJSX: true
+      }
+    },
     '@storybook/addon-actions',
     '@storybook/addon-links',
   ],
